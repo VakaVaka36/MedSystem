694ba0e3ad06f4accc1b0b90f7f07826bfe761e9
Егор <@DESKTOP-3UQG7N8>
2022-04-26 13:47:03 +0300

    Добавьте файлы проекта.

---
+++ MedSystem/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace MedSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "<57",
+            "57-59",
+            "60-62",
+            "63-65",
+            "66-68",
+            "69-71",
+            "72-74",
+            "75-77",
+            "78-80",
+            "81-83",
+            "84-86"});
+            this.comboBox1.Location = new System.Drawing.Point(151, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(83, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Возраст";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(36, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(626, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Оценка индивидуального риска развития инфаркта миокарда в ближайшие 10 лет.";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Мужской",
+            "Женский"});
+            this.comboBox2.Location = new System.Drawing.Point(39, 66);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(83, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.Text = "Пол";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Общий холестерин";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "<0,9",
+            "0,9-1,6",
+            "1,17-1,29",
+            "1,30-1,55",
+            ">1,56"});
+            this.comboBox3.Location = new System.Drawing.Point(151, 104);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(83, 21);
+            this.comboBox3.TabIndex = 4;
+            this.comboBox3.Text = "ммоль/л";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Систолическое АД";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(151, 141);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(83, 21);
+            this.comboBox4.TabIndex = 6;
+            this.comboBox4.Text = "мм рт. ст.";
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 38);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Расчитать риск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(279, 66);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Сахарный диабет";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(279, 89);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(68, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Курение";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(279, 112);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(207, 17);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "Атеросклеротическое заболевание";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(279, 135);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(188, 17);
+            this.checkBox4.TabIndex = 12;
+            this.checkBox4.Text = "Гипертрофия левого желудочка";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(276, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Уровень риска:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(192, 33);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Обновить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(371, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 289);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "Простейшая медицинская экспертная система";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MedSystem/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedSystem
+{
+    public partial class Form1 : Form
+    {
+        System.Object[] ItemObject = new System.Object[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        int ball = 0;
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "Мужской")
+            {
+                comboBox4.Items.Clear();
+                comboBox4.Items.Insert(0, "95-105");
+                comboBox4.Items.Insert(1, "106-116");
+                comboBox4.Items.Insert(2, "117-126");
+                comboBox4.Items.Insert(3, "127-137");
+                comboBox4.Items.Insert(4, "138-148");
+                comboBox4.Items.Insert(5, "149-159");
+                comboBox4.Items.Insert(6, "160-170");
+                comboBox4.Items.Insert(7, "171-181");
+                comboBox4.Items.Insert(8, "182-191");
+                comboBox4.Items.Insert(9, "192-202");
+                comboBox4.Items.Insert(10, "203-213");
+            }
+            if (comboBox2.Text == "Женский")
+            {
+                comboBox4.Items.Clear();
+                comboBox4.Items.Insert(0, "95-104");
+                comboBox4.Items.Insert(1, "105-114");
+                comboBox4.Items.Insert(2, "115-124");
+                comboBox4.Items.Insert(3, "125-134");
+                comboBox4.Items.Insert(4, "135-144");
+                comboBox4.Items.Insert(5, "145-154");
+                comboBox4.Items.Insert(6, "155-164");
+                comboBox4.Items.Insert(7, "165-174");
+                comboBox4.Items.Insert(8, "175-184");
+                comboBox4.Items.Insert(9, "185-194");
+                comboBox4.Items.Insert(10, "195-204");
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Возраст
+            if (comboBox1.Text == "57-59")
+                ball += 1;
+            else if (comboBox1.Text == "60-62")
+                ball += 2;
+            else if (comboBox1.Text == "63-65")
+                ball += 3;
+            else if (comboBox1.Text == "66-68")
+                ball += 4;
+            else if (comboBox1.Text == "69-71")
+                ball += 5;
+            else if (comboBox1.Text == "72-74")
+                ball += 6;
+            else if (comboBox1.Text == "75-77")
+                ball += 7;
+            else if (comboBox1.Text == "78-80")
+                ball += 8;
+            else if (comboBox1.Text == "81-83")
+                ball += 9;
+            else if (comboBox1.Text == "84-86")
+                ball += 10;
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Холестерин
+            if (comboBox3.Text == "<0,9" && comboBox2.Text == "Мужской")
+                ball += 3;
+            else if (comboBox3.Text == "<0,9" && comboBox2.Text == "Женский")
+                ball += 5;
+            else if (comboBox3.Text == "0,9-1,6" && comboBox2.Text == "Мужской")
+                ball += 2;
+            else if (comboBox3.Text == "0,9-1,6" && comboBox2.Text == "Женский")
+                ball += 2;
+            else if (comboBox3.Text == "1,17-1,29" && comboBox2.Text == "Мужской")
+                ball += 1;
+            else if (comboBox3.Text == ">1,56" && comboBox2.Text == "Женский")
+                ball -= 2;
+            else if (comboBox3.Text == ">1,56" && comboBox2.Text == "Мужской")
+                ball -= 3;
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Систолическое АД мужчины
+            if (comboBox4.Text == "106-116")
+                ball += 1;
+            else if (comboBox4.Text == "117-126")
+                ball += 2;
+            else if (comboBox4.Text == "127-137")
+                ball += 3;
+            else if (comboBox4.Text == "138-148")
+                ball += 4;
+            else if (comboBox4.Text == "149-159")
+                ball += 5;
+            else if (comboBox4.Text == "160-170")
+                ball += 6;
+            else if (comboBox4.Text == "171-181")
+                ball += 7;
+            else if (comboBox4.Text == "182-191")
+                ball += 8;
+            else if (comboBox4.Text == "192-202")
+                ball += 9;
+            else if (comboBox4.Text == "203-213")
+                ball += 10;
+
+            //Систолическое АД женщины
+            if (comboBox4.Text == "105-114")
+                ball += 1;
+            else if (comboBox4.Text == "115-124")
+                ball += 2;
+            else if (comboBox4.Text == "125-134")
+                ball += 3;
+            else if (comboBox4.Text == "135-144")
+                ball += 4;
+            else if (comboBox4.Text == "145-154")
+                ball += 5;
+            else if (comboBox4.Text == "155-164")
+                ball += 6;
+            else if (comboBox4.Text == "165-174")
+                ball += 7;
+            else if (comboBox4.Text == "175-184")
+                ball += 8;
+            else if (comboBox4.Text == "185-194")
+                ball += 9;
+            else if (comboBox4.Text == "195-204")
+                ball += 10;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //Сахарный диабет
+            if (checkBox1.Checked == true && comboBox2.Text == "Мужской")
+                ball += 2;
+            else if (checkBox1.Checked == true && comboBox2.Text == "Женский")
+                ball += 3;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            //Курение
+            if (checkBox2.Checked == true && comboBox2.Text == "Мужской")
+                ball += 3;
+            else if (checkBox2.Checked == true && comboBox2.Text == "Женский")
+                ball += 3;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            //Атеросклеротическое заболевание
+            if (checkBox3.Checked == true && comboBox2.Text == "Мужской")
+                ball += 3;
+            else if (checkBox3.Checked == true && comboBox2.Text == "Женский")
+                ball += 2;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            //Гипертрофия левого желудочка
+            if (checkBox4.Checked == true && comboBox2.Text == "Мужской")
+                ball += 6;
+            else if (checkBox4.Checked == true && comboBox2.Text == "Женский")
+                ball += 4;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ball <= 10 && comboBox2.Text == "Мужской")
+            {
+                label4.ForeColor = Color.Green;
+                label4.Text = "низкий - менее 10%";
+            }
+
+            else if (ball >= 10 && ball <= 14 && comboBox2.Text == "Мужской")
+            {
+                label4.ForeColor = Color.Yellow;
+                label4.Text = "промежуточный - 10-15%";
+            }
+
+            else if (ball >= 13 && ball <= 15 && comboBox2.Text == "Мужской")
+            {
+                label4.ForeColor = Color.Orange;
+                label4.Text = "высокий - 16-20%";
+            }
+
+            else if (ball >= 16 && ball <= 100 && comboBox2.Text == "Мужской")
+            {
+                label4.ForeColor = Color.Red;
+                label4.Text = "очень высокий - более 20%";
+            }
+
+            //Женский
+             else if (ball <= 12 && comboBox2.Text == "Женский")
+            {
+                label4.ForeColor = Color.Green;
+                label4.Text = "низкий - менее 10%";
+            }
+
+            else if (ball >= 13 && ball <= 14 && comboBox2.Text == "Женский")
+            {
+                label4.ForeColor = Color.Yellow;
+                label4.Text = "промежуточный - 10-15%";
+            }
+
+            else if (ball >= 15 && ball <= 16 && comboBox2.Text == "Женский")
+            {
+                label4.ForeColor = Color.Orange;
+                label4.Text = "высокий - 16-20%";
+            }
+
+            else if (ball >= 17 && ball <= 100 && comboBox2.Text == "Женский")
+            {
+                label4.ForeColor = Color.Red;
+                label4.Text = "очень высокий - более 20%";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
